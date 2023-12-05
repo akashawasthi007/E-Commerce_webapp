f6197e0309de3558d4cb1eb94416772453150650
Akash Awasthi <akashawasthi076@gmail.com>
2023-12-05 16:46:19 +0530

    logout function implemented

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,11 +1,39 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {
-// value : string | undefined;
-value="Hello";
+  // value : string | undefined;
+  constructor(private route: Router) {}
+
+  value = 'Hello';
+  menuType: string = 'default';
+  sellerName :string ="";
+
+  ngOnInit(): void {
+    this.route.events.subscribe((val: any) => {
+      console.log(val.url);
+      if (val.url) {
+        if (localStorage.getItem('seller') && val.url.includes('seller')) {
+          this.menuType = 'seller';
+          let sellerstorage = localStorage.getItem('seller');
+          let sellerdata = sellerstorage && JSON.parse(sellerstorage)[0];
+          console.log(sellerdata);
+          //this.sellerName=sellerdata.name;
+        } else {
+          this.menuType = 'default';
+        }
+      }
+    });
+  }
+
+  logout():void{
+    console.log("hello")
+    localStorage.removeItem('seller');
+    this.route.navigate(['/']);
+  }
 }
